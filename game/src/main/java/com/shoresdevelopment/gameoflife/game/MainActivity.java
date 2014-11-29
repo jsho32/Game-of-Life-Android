32b6268922a3ab8bdc3d4950e57f5a7ee7928953
jsho32 <justin.shores32@gmail.com>
2014-11-29 03:24:57 -0800

    Link textviews to respective activities.

--- game/src/main/java/com/shoresdevelopment/gameoflife/game/MainActivity.java
+++ game/src/main/java/com/shoresdevelopment/gameoflife/game/MainActivity.java
@@ -1,17 +1,23 @@
 package com.shoresdevelopment.gameoflife.game;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
 
 
 public class MainActivity extends ActionBarActivity {
+    TextView start;
+    TextView about;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initializeTextViews();
     }
 
 
@@ -35,5 +41,24 @@ public class MainActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    private void initializeTextViews() {
+        start = (TextView) findViewById(R.id.start);
+        start.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, GameActivity.class);
+                startActivity(intent);
+            }
+        });
+        about = (TextView) findViewById(R.id.about);
+        about.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, AboutActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
