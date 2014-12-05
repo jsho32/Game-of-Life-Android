59181d204922cbfaf6797ca94bc05a027c8990d6
jsho32 <justin.shores32@gmail.com>
2014-12-04 23:05:11 -0800

    Create thread to run generation iterations.

--- game/src/main/java/com/shoresdevelopment/gameoflife/game/GameActivity.java
+++ game/src/main/java/com/shoresdevelopment/gameoflife/game/GameActivity.java
@@ -3,25 +3,51 @@ package com.shoresdevelopment.gameoflife.game;
 import android.app.Activity;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
 import android.widget.GridLayout;
 import android.widget.GridView;
+import android.widget.TextView;
 
 public class GameActivity extends Activity{
     private int boardSize = 400;
     private int[] boardArray;
     private GridView lifeGrid;
+    private TextView gameStart;
+    private TextView gameStop;
+    private boolean isRunning;
+    GameGridAdapter gridAdapter;
+    LifeManager lifeManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
+        initializeButtons();
         fillGridArray();
         initializeGrid();
+        lifeManager = new LifeManager(lifeGrid, gridAdapter);
+    }
+
+    /** runs on the thread */
+    private void gameRun() {
+        final Handler handler = new Handler();
+        final Runnable runnable = new Runnable() {
+            @Override
+            public void run() {
+                if (isRunning) {
+                    lifeManager.setLife();
+                    //lifeGrid = lifeManager.getGridView();
+                    handler.postDelayed(this, 1000);
+                }
+            }
+        };
+        handler.post(runnable);
     }
 
     private void initializeGrid() {
         lifeGrid = (GridView) findViewById(R.id.lifeGrid);
-        GameGridAdapter gridAdapter = new GameGridAdapter(GameActivity.this, boardArray);
+        gridAdapter = new GameGridAdapter(GameActivity.this, boardArray);
         lifeGrid.setAdapter(gridAdapter);
         lifeGrid.setBackgroundColor(Color.BLACK);
         lifeGrid.setVerticalSpacing(1);
@@ -33,5 +59,31 @@ public class GameActivity extends Activity{
         for (int i = 0; i < boardSize; i++) {
             boardArray[i] = 0;
         }
+    }
+
+    private void initializeButtons() {
+        gameStart = (TextView) findViewById(R.id.game_start);
+        gameStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                isRunning = true;
+                gameRun();
+                gameStart.setBackgroundColor(Color.GREEN);
+                gameStart.setClickable(false);
+                gameStop.setClickable(true);
+                gameStop.setBackgroundColor(Color.BLACK);
+            }
+        });
+        gameStop = (TextView) findViewById(R.id.game_stop);
+        gameStop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                isRunning = false;
+                gameStop.setBackgroundColor(Color.RED);
+                gameStop.setClickable(false);
+                gameStart.setClickable(true);
+                gameStart.setBackgroundColor(Color.BLACK);
+            }
+        });
     }
 }
