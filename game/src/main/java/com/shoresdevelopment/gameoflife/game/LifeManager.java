83164b68025898e5afd49d5f2a014be89044b52e
jsho32 <justin.shores32@gmail.com>
2014-12-16 20:54:17 -0800

    Implement the game of life algorithm.

--- game/src/main/java/com/shoresdevelopment/gameoflife/game/LifeManager.java
+++ game/src/main/java/com/shoresdevelopment/gameoflife/game/LifeManager.java
@@ -1,24 +1,106 @@
 package com.shoresdevelopment.gameoflife.game;
 
+import android.util.Log;
 import android.widget.GridView;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class LifeManager {
     private GridView gridView;
     private GameGridAdapter gridAdapter;
+    private int columnCount = 20;
+    private int boardsize = 400;
     private int position = 0;
+    private Map<Integer, Integer> cells;
 
     public LifeManager(GridView gridView, GameGridAdapter gridAdapter) {
         this.gridView = gridView;
         this.gridAdapter = gridAdapter;
+        initializeCellsMap();
+    }
+
+    private void initializeCellsMap() {
+        cells = new HashMap<Integer, Integer>();
+        for (int i = 0; i < boardsize; i++) {
+            cells.put(i, gridAdapter.getPositionValue(i));
+        }
     }
 
     public void setLife() {
-        position += 1;
-        gridAdapter.setPositionValue(position, 1);
-        gridAdapter.getView(position, gridView.getChildAt(position), gridView);
+        initializeCellsMap();
+        getNewLife();
+        for (int i = 0; i < boardsize; i++) {
+            gridAdapter.setPositionValue(i, cells.get(i));
+            gridAdapter.getView(i, gridView.getChildAt(i), gridView);
+        }
     }
 
-    public GridView getGridView() {
-        return this.gridView;
+    public void getNewLife() {
+        for(int i = 0; i < boardsize; i++) {
+            int count = 0;
+            if (i == 0) {
+                if (gridAdapter.getPositionValue(i + 1) == 1) count++;
+                if (gridAdapter.getPositionValue(i + columnCount) == 1) count++;
+                if (gridAdapter.getPositionValue(i + columnCount + 1) == 1) count++;
+            } else if (i < columnCount - 1) {
+                if (gridAdapter.getPositionValue(i - 1) == 1) count++;
+                if (gridAdapter.getPositionValue(i + 1) == 1) count++;
+                if (gridAdapter.getPositionValue(i + columnCount - 1) == 1) count++;
+                if (gridAdapter.getPositionValue(i + columnCount) == 1) count++;
+                if (gridAdapter.getPositionValue(i + columnCount + 1) == 1) count++;
+            } else if (i == columnCount - 1) {
+                if (gridAdapter.getPositionValue(i - 1) == 1) count++;
+                if (gridAdapter.getPositionValue(i + columnCount - 1) == 1) count++;
+                if (gridAdapter.getPositionValue(i + columnCount) == 1) count++;
+            } else if (i == boardsize - columnCount) {
+                if (gridAdapter.getPositionValue(i - columnCount) == 1) count++;
+                if (gridAdapter.getPositionValue(i - columnCount + 1) == 1) count++;
+                if (gridAdapter.getPositionValue(i + 1) == 1) count++;
+            } else if (i > boardsize - columnCount && i < boardsize - 1) {
+                if (gridAdapter.getPositionValue(i - columnCount - 1) == 1) count++;
+                if (gridAdapter.getPositionValue(i - columnCount) == 1) count++;
+                if (gridAdapter.getPositionValue(i - columnCount + 1) == 1) count++;
+                if (gridAdapter.getPositionValue(i - 1) == 1) count++;
+                if (gridAdapter.getPositionValue(i + 1) == 1) count++;
+            } else if (i == boardsize - 1) {
+                if (gridAdapter.getPositionValue(i - columnCount) == 1) count++;
+                if (gridAdapter.getPositionValue(i - columnCount - 1) == 1) count++;
+                if (gridAdapter.getPositionValue(i - 1) == 1) count++;
+            } else if (i % columnCount == 0) {
+                if (gridAdapter.getPositionValue(i - columnCount) == 1) count++;
+                if (gridAdapter.getPositionValue(i - columnCount + 1) == 1) count++;
+                if (gridAdapter.getPositionValue(i + 1) == 1) count++;
+                if (gridAdapter.getPositionValue(i + columnCount) == 1) count++;
+                if (gridAdapter.getPositionValue(i + columnCount + 1) == 1) count++;
+            } else if (i % columnCount == columnCount - 1) {
+                if (gridAdapter.getPositionValue(i - columnCount) == 1) count++;
+                if (gridAdapter.getPositionValue(i - columnCount - 1) == 1) count++;
+                if (gridAdapter.getPositionValue(i - 1) == 1) count++;
+                if (gridAdapter.getPositionValue(i + columnCount - 1) == 1) count++;
+                if (gridAdapter.getPositionValue(i + columnCount) == 1) count++;
+            } else {
+                if (gridAdapter.getPositionValue(i - columnCount - 1) == 1) count++;
+                if (gridAdapter.getPositionValue(i - columnCount) == 1) count++;
+                if (gridAdapter.getPositionValue(i - columnCount + 1) == 1) count++;
+                if (gridAdapter.getPositionValue(i - 1) == 1) count++;
+                if (gridAdapter.getPositionValue(i + 1) == 1) count++;
+                if (gridAdapter.getPositionValue(i + columnCount - 1) == 1) count++;
+                if (gridAdapter.getPositionValue(i + columnCount) == 1) count++;
+                if (gridAdapter.getPositionValue(i + columnCount + 1) == 1) count++;
+            }
+
+            if (cells.get(i) == 1) {
+                if (count != 2 && count != 3) {
+                    cells.put(i, 0);
+                    Log.e(String.valueOf(i) + " is ", String.valueOf(cells.get(i)));
+                }
+            } else {
+                if (count == 3) {
+                    cells.put(i, 1);
+                    Log.e(String.valueOf(i) + " is ", String.valueOf(cells.get(i)));
+                }
+            }
+        }
     }
 }
