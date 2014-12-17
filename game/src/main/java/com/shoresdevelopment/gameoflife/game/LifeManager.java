2813f3529c171a20cdf44002a1c8f67a0ecfeeb1
jsho32 <justin.shores32@gmail.com>
2014-12-16 21:16:55 -0800

    Clean code/Remove Logs.

--- game/src/main/java/com/shoresdevelopment/gameoflife/game/LifeManager.java
+++ game/src/main/java/com/shoresdevelopment/gameoflife/game/LifeManager.java
@@ -1,6 +1,5 @@
 package com.shoresdevelopment.gameoflife.game;
 
-import android.util.Log;
 import android.widget.GridView;
 
 import java.util.HashMap;
@@ -9,20 +8,18 @@ import java.util.Map;
 public class LifeManager {
     private GridView gridView;
     private GameGridAdapter gridAdapter;
-    private int columnCount = 20;
-    private int boardsize = 400;
-    private int position = 0;
+    private final int columnCount = 20;
+    private final int boardSize = 400;
     private Map<Integer, Integer> cells;
 
     public LifeManager(GridView gridView, GameGridAdapter gridAdapter) {
         this.gridView = gridView;
         this.gridAdapter = gridAdapter;
-        initializeCellsMap();
     }
 
     private void initializeCellsMap() {
         cells = new HashMap<Integer, Integer>();
-        for (int i = 0; i < boardsize; i++) {
+        for (int i = 0; i < boardSize; i++) {
             cells.put(i, gridAdapter.getPositionValue(i));
         }
     }
@@ -30,14 +27,14 @@ public class LifeManager {
     public void setLife() {
         initializeCellsMap();
         getNewLife();
-        for (int i = 0; i < boardsize; i++) {
+        for (int i = 0; i < boardSize; i++) {
             gridAdapter.setPositionValue(i, cells.get(i));
             gridAdapter.getView(i, gridView.getChildAt(i), gridView);
         }
     }
 
     public void getNewLife() {
-        for(int i = 0; i < boardsize; i++) {
+        for(int i = 0; i < boardSize; i++) {
             int count = 0;
             if (i == 0) {
                 if (gridAdapter.getPositionValue(i + 1) == 1) count++;
@@ -53,17 +50,17 @@ public class LifeManager {
                 if (gridAdapter.getPositionValue(i - 1) == 1) count++;
                 if (gridAdapter.getPositionValue(i + columnCount - 1) == 1) count++;
                 if (gridAdapter.getPositionValue(i + columnCount) == 1) count++;
-            } else if (i == boardsize - columnCount) {
+            } else if (i == boardSize - columnCount) {
                 if (gridAdapter.getPositionValue(i - columnCount) == 1) count++;
                 if (gridAdapter.getPositionValue(i - columnCount + 1) == 1) count++;
                 if (gridAdapter.getPositionValue(i + 1) == 1) count++;
-            } else if (i > boardsize - columnCount && i < boardsize - 1) {
+            } else if (i > boardSize - columnCount && i < boardSize - 1) {
                 if (gridAdapter.getPositionValue(i - columnCount - 1) == 1) count++;
                 if (gridAdapter.getPositionValue(i - columnCount) == 1) count++;
                 if (gridAdapter.getPositionValue(i - columnCount + 1) == 1) count++;
                 if (gridAdapter.getPositionValue(i - 1) == 1) count++;
                 if (gridAdapter.getPositionValue(i + 1) == 1) count++;
-            } else if (i == boardsize - 1) {
+            } else if (i == boardSize - 1) {
                 if (gridAdapter.getPositionValue(i - columnCount) == 1) count++;
                 if (gridAdapter.getPositionValue(i - columnCount - 1) == 1) count++;
                 if (gridAdapter.getPositionValue(i - 1) == 1) count++;
@@ -93,12 +90,10 @@ public class LifeManager {
             if (cells.get(i) == 1) {
                 if (count != 2 && count != 3) {
                     cells.put(i, 0);
-                    Log.e(String.valueOf(i) + " is ", String.valueOf(cells.get(i)));
                 }
             } else {
                 if (count == 3) {
                     cells.put(i, 1);
-                    Log.e(String.valueOf(i) + " is ", String.valueOf(cells.get(i)));
                 }
             }
         }
