09155bb365b3511512874e51a8fb35fd5f4a375e
jsho32 <justin.shores32@gmail.com>
2014-12-20 04:36:04 -0800

    Allow user to change speed of evolution and board size.

--- game/src/main/java/com/shoresdevelopment/gameoflife/game/GameActivity.java
+++ game/src/main/java/com/shoresdevelopment/gameoflife/game/GameActivity.java
@@ -4,29 +4,51 @@ import android.app.Activity;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.os.Handler;
+import android.util.Log;
 import android.view.View;
 import android.widget.GridView;
+import android.widget.PopupWindow;
 import android.widget.TextView;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class GameActivity extends Activity{
-    private final int boardSize = 400;
+    private Map<Integer, Integer> boardSizeMap;
+    private Map<Integer, Integer> speedMap;
+    private int boardSize;
+    private int boardKey;
+    private int evolutionSpeed;
+    private int evolutionKey;
     private int[] boardArray;
     private GridView lifeGrid;
     private TextView gameStart;
     private TextView gameStop;
+    private TextView size;
+    private TextView speed;
+    private TextView shapes;
     private boolean isRunning;
-    GameGridAdapter gridAdapter;
-    LifeManager lifeManager;
+    private List<String> sizeList;
+    private List<String> speedList;
+    private GameGridAdapter gridAdapter;
+    private LifeManager lifeManager;
 
     /** Called when activity is first created */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
-        initializeButtons();
+        initializeGameButtons();
+        setLists();
+        setMaps();
+        setBoardSize(boardSizeMap.get(1));
+        setSpeed(speedMap.get(1));
+        initializeFeaturesButtons();
         fillGridArray();
         initializeGrid();
-        lifeManager = new LifeManager(lifeGrid, gridAdapter);
+        lifeManager = new LifeManager(lifeGrid, gridAdapter, boardSize, boardSizeMap.get(boardKey));
     }
 
     /** runs on the thread */
@@ -37,19 +59,72 @@ public class GameActivity extends Activity{
             public void run() {
                 if (isRunning) {
                     lifeManager.setLife();
-                    handler.postDelayed(this, 1000);
+                    handler.postDelayed(this, evolutionSpeed);
                 }
             }
         };
         handler.post(runnable);
     }
 
+    /** Set the values of features lists (board sizes, generation speeds) */
+    private void setLists() {
+        sizeList = new ArrayList<String>();
+        sizeList.add("10 X 10");
+        sizeList.add("20 X 20");
+        sizeList.add("50 X 50");
+        sizeList.add("100 X 100");
+
+        speedList = new ArrayList<String>();
+        speedList.add("Really Slow");
+        speedList.add("Slow");
+        speedList.add("Medium");
+        speedList.add("Fast");
+        speedList.add("Really Fast");
+    }
+
+    /** Set the values of features maps (board sizes, generation speeds) */
+    private void setMaps() {
+        boardSizeMap = new HashMap<Integer, Integer>();
+        boardSizeMap.put(0, 10);
+        boardSizeMap.put(1, 20);
+        boardSizeMap.put(2, 50);
+        boardSizeMap.put(3, 100);
+
+        speedMap = new HashMap<Integer, Integer>();
+        speedMap.put(0, 3000);
+        speedMap.put(1, 2000);
+        speedMap.put(2, 1000);
+        speedMap.put(3, 500);
+        speedMap.put(4, 200);
+    }
+
+    /** Sets the board size and board key */
+    private void setBoardSize(int size) {
+        boardSize = (int) Math.pow(size, 2);
+        for (Map.Entry<Integer, Integer> entry : boardSizeMap.entrySet()) {
+            if (size == entry.getValue()) {
+                boardKey = entry.getKey();
+            }
+        }
+    }
+
+    /** Sets the speed for generation iterations */
+    private void setSpeed(int speed) {
+        evolutionSpeed = speed;
+        for (Map.Entry<Integer, Integer> entry : speedMap.entrySet()) {
+            if (speed == entry.getValue()) {
+                evolutionKey = entry.getKey();
+            }
+        }
+    }
+
     /** Sets initial settings for the grid that contains life cells */
     private void initializeGrid() {
         lifeGrid = (GridView) findViewById(R.id.lifeGrid);
-        gridAdapter = new GameGridAdapter(GameActivity.this, boardArray);
+        gridAdapter = new GameGridAdapter(GameActivity.this, boardArray, boardSizeMap.get(boardKey));
         lifeGrid.setAdapter(gridAdapter);
         lifeGrid.setBackgroundColor(Color.BLACK);
+        lifeGrid.setNumColumns(boardSizeMap.get(boardKey));
         lifeGrid.setVerticalSpacing(1);
         lifeGrid.setHorizontalSpacing(1);
     }
@@ -62,8 +137,8 @@ public class GameActivity extends Activity{
         }
     }
 
-    /** Initialize all buttons and sets on click listeners */
-    private void initializeButtons() {
+    /** Initializes game buttons and sets on click listeners */
+    private void initializeGameButtons() {
         gameStart = (TextView) findViewById(R.id.game_start);
         gameStart.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -91,6 +166,48 @@ public class GameActivity extends Activity{
                 gameStart.setBackgroundColor(Color.BLACK);
                 gameStart.setTextColor(Color.WHITE);
                 setGridClickable(true);
+            }
+        });
+    }
+
+    /** Initializes features buttons and sets on click listeners */
+    private void initializeFeaturesButtons() {
+        final DropDownManager sizesDropDown = new DropDownManager(GameActivity.this, sizeList, boardKey);
+        size = (TextView) findViewById(R.id.board_size);
+        size.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                size.setBackgroundColor(Color.GRAY);
+                sizesDropDown.popupWindow.showAsDropDown(v, -5, 0);
+                sizesDropDown.popupWindow.setOnDismissListener(new PopupWindow.OnDismissListener() {
+                    @Override
+                    public void onDismiss() {
+                        size.setBackgroundColor(Color.BLACK);
+                        boardKey = sizesDropDown.getSelected();
+                        setBoardSize(boardSizeMap.get(boardKey));
+                        fillGridArray();
+                        initializeGrid();
+                        lifeManager = new LifeManager(lifeGrid, gridAdapter, boardSize, boardSizeMap.get(boardKey));
+                    }
+                });
+            }
+        });
+
+        final DropDownManager speedDropDown = new DropDownManager(GameActivity.this, speedList, boardKey);
+        speed = (TextView) findViewById(R.id.generation_speed);
+        speed.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                speed.setBackgroundColor(Color.GRAY);
+                speedDropDown.popupWindow.showAsDropDown(v, -5, 0);
+                speedDropDown.popupWindow.setOnDismissListener(new PopupWindow.OnDismissListener() {
+                    @Override
+                    public void onDismiss() {
+                        speed.setBackgroundColor(Color.BLACK);
+                        evolutionKey = speedDropDown.getSelected();
+                        setSpeed(speedMap.get(evolutionKey));
+                    }
+                });
             }
         });
     }
