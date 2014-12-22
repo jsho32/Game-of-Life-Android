57c522f41119361940c8342589ef7e2c51de483e
jsho32 <justin.shores32@gmail.com>
2014-12-22 04:31:18 -0800

    Create shapes manager, implement simple cross shape.

---
+++ game/src/main/java/com/shoresdevelopment/gameoflife/game/ShapesManager.java
@@ -0,0 +1,41 @@
+package com.shoresdevelopment.gameoflife.game;
+
+
+import android.util.Log;
+import android.widget.GridView;
+
+public class ShapesManager {
+    private GridView gridView;
+    private GameGridAdapter gridAdapter;
+    private int columnCount;
+    private int boardSize;
+
+    public ShapesManager(GridView gridView, GameGridAdapter gridAdapter, int boardSize, int columnCount) {
+        this.gridView = gridView;
+        this.gridAdapter = gridAdapter;
+        this.columnCount = columnCount;
+        this.boardSize = boardSize;
+    }
+
+    public void getShapeToAdd(String shape) {
+        if (shape.equals("Cross")) {
+            addCross();
+        }
+    }
+
+    private void addCross() {
+        int position = columnCount / 2;
+        while (position < boardSize) {
+            gridAdapter.setPositionValue(position, 1);
+            gridAdapter.getView(position, gridView.getChildAt(position), gridView);
+            position += columnCount;
+        }
+        position = boardSize / 2;
+        while (position < ((boardSize/2) + columnCount)) {
+            Log.e("Position ", String.valueOf(position));
+            gridAdapter.setPositionValue(position, 1);
+            gridAdapter.getView(position, gridView.getChildAt(position), gridView);
+            position += 1;
+        }
+    }
+}
