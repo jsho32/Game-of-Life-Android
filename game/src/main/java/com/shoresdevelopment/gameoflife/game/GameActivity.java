b5702e0dce205c489345bb3043ce4f23d775a07d
jsho32 <justin.shores32@gmail.com>
2015-02-12 03:04:38 -0800

    Do not clear board if board size is not changed.

--- game/src/main/java/com/shoresdevelopment/gameoflife/game/GameActivity.java
+++ game/src/main/java/com/shoresdevelopment/gameoflife/game/GameActivity.java
@@ -24,11 +24,15 @@ import java.util.Map;
 public class GameActivity extends Activity {
     private Map<Integer, Integer> boardSizeMap;
     private Map<Integer, Integer> speedMap;
+    private Map<Integer, Integer> cellColorsMap;
+    private Map<Integer, Integer> bgColorsMap;
     private int buttonSize, buttonPadding;
     private int boardSize;
     private int boardKey;
     private int evolutionSpeed;
     private int evolutionKey;
+    private int cellColorKey;
+    private int bgColorKey;
     private int[] boardArray;
     private GridView lifeGrid;
     private ImageView gameStart;
@@ -37,6 +41,10 @@ public class GameActivity extends Activity {
     private List<String> sizeList;
     private List<String> speedList;
     private List<String> shapeList;
+    private List<String> aboutList;
+    private List<String> settingsList;
+    private List<String> cellColorsList;
+    private List<String> bgColorsList;
     private GameGridAdapter gridAdapter;
     private LifeManager lifeManager;
     private LinearLayout.LayoutParams params;
@@ -50,14 +58,15 @@ public class GameActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
 
+        setLists();
+        setMaps();
         setButtonSize();
         initializeHeader();
         initializeGameButtons();
-        setLists();
-        setMaps();
 
         setBoardSize(boardSizeMap.get(1));
         setSpeed(speedMap.get(2));
+        setCellColor(cellColorsMap.get(5));
 
         initializeFeaturesButtons();
         fillGridArray();
@@ -148,6 +157,37 @@ public class GameActivity extends Activity {
         shapeList.add("Small Square");
         shapeList.add("X");
         shapeList.add("Border");
+
+        aboutList = new ArrayList<>();
+        aboutList.add("About The Game of Life:");
+        aboutList.add("Game Information");
+        aboutList.add("More from Developer!");
+
+        settingsList = new ArrayList<>();
+        settingsList.add("Select a settings group:");
+        settingsList.add("Cell color");
+        settingsList.add("Background color");
+
+        cellColorsList = new ArrayList<>();
+        cellColorsList.add("Select a cell color:");
+        cellColorsList.add("Black");
+        cellColorsList.add("Blue");
+        cellColorsList.add("Gold");
+        cellColorsList.add("Green");
+        cellColorsList.add("Purple");
+        cellColorsList.add("Red");
+        cellColorsList.add("White");
+
+        bgColorsList = new ArrayList<>();
+        bgColorsList.add("Select a background color:");
+        bgColorsList.add("Black");
+        bgColorsList.add("Blue");
+        bgColorsList.add("Cyan");
+        bgColorsList.add("Gray");
+        bgColorsList.add("Green");
+        bgColorsList.add("Red");
+        bgColorsList.add("White");
+        bgColorsList.add("Yellow");
     }
 
     /**
@@ -170,13 +210,32 @@ public class GameActivity extends Activity {
         speedMap.put(2, 1000);
         speedMap.put(3, 500);
         speedMap.put(4, 200);
+
+        cellColorsMap = new HashMap<>();
+        cellColorsMap.put(0, R.drawable.black_circle);
+        cellColorsMap.put(1, R.drawable.blue_circle);
+        cellColorsMap.put(2, R.drawable.gold_circle);
+        cellColorsMap.put(3, R.drawable.green_circle);
+        cellColorsMap.put(4, R.drawable.purple_circle);
+        cellColorsMap.put(5, R.drawable.red_circle);
+        cellColorsMap.put(6, R.drawable.white_circle);
+
+        bgColorsMap = new HashMap<>();
+        bgColorsMap.put(0, Color.BLACK);
+        bgColorsMap.put(1, Color.BLUE);
+        bgColorsMap.put(2, Color.CYAN);
+        bgColorsMap.put(3, Color.GRAY);
+        bgColorsMap.put(4, Color.GREEN);
+        bgColorsMap.put(5, Color.RED);
+        bgColorsMap.put(6, Color.WHITE);
+        bgColorsMap.put(7, Color.YELLOW);
     }
 
     /**
      * Sets the board size and board key
      */
     private void setBoardSize(int size) {
-        boardSize = (int) Math.pow(size, 2);
+        boardSize = size * size;
 
         for (Map.Entry<Integer, Integer> entry : boardSizeMap.entrySet()) {
 
@@ -205,13 +264,26 @@ public class GameActivity extends Activity {
     }
 
     /**
+     * Sets the image for the cells
+     */
+    private void setCellColor(int image) {
+        for (Map.Entry<Integer, Integer> entry : cellColorsMap.entrySet()) {
+
+            if (image == entry.getValue()) {
+                cellColorKey = entry.getKey();
+            }
+
+        }
+
+    }
+
+    /**
      * Sets initial settings for the grid that contains life cells
      */
     private void initializeGrid() {
         lifeGrid = (GridView) findViewById(R.id.lifeGrid);
-        gridAdapter = new GameGridAdapter(GameActivity.this, boardArray, boardSizeMap.get(boardKey));
+        gridAdapter = new GameGridAdapter(GameActivity.this, boardArray, boardSizeMap.get(boardKey), cellColorsMap.get(cellColorKey));
         lifeGrid.setAdapter(gridAdapter);
-        lifeGrid.setBackgroundColor(Color.BLACK);
         lifeGrid.setNumColumns(boardSizeMap.get(boardKey));
     }
 
@@ -231,6 +303,8 @@ public class GameActivity extends Activity {
      * Initializes the header components
      */
     private void initializeHeader() {
+        final DropDownManager aboutDropDown = new DropDownManager(GameActivity.this, aboutList, aboutList.size());
+
         ImageView about = (ImageView) findViewById(R.id.about);
         RelativeLayout.LayoutParams headerParams = new RelativeLayout.LayoutParams(buttonSize, buttonSize);
         headerParams.setMargins(buttonPadding, buttonPadding * 2, buttonPadding, buttonPadding * 2);
@@ -239,11 +313,26 @@ public class GameActivity extends Activity {
         about.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Uri uri = Uri.parse("http://en.wikipedia.org/wiki/Conway%27s_Game_of_Life");
-                Intent intent = new Intent(Intent.ACTION_VIEW, uri);
-                startActivity(intent);
-            }
+                aboutDropDown.popupWindow.showAtLocation(v, Gravity.CENTER, 0, 0);
+                aboutDropDown.popupWindow.setOnDismissListener(new PopupWindow.OnDismissListener() {
+                    @Override
+                    public void onDismiss() {
 
+                        if (aboutDropDown.getSelected() == 1) {
+                            Uri uri = Uri.parse("http://en.wikipedia.org/wiki/Conway%27s_Game_of_Life");
+                            Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+                            startActivity(intent);
+                        } else if (aboutDropDown.getSelected() == 2) {
+                            Uri uri = Uri.parse("https://play.google.com/store/apps/developer?id=J.+Shores+Development");
+                            Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+                            startActivity(intent);
+                        }
+
+                        aboutDropDown.setCurrentSelection(aboutList.size());
+
+                    }
+                });
+            }
         });
 
         ImageView header = (ImageView) findViewById(R.id.header_banner);
@@ -252,15 +341,67 @@ public class GameActivity extends Activity {
         headerParams.addRule(RelativeLayout.CENTER_IN_PARENT);
         header.setLayoutParams(headerParams);
 
+        final DropDownManager settingsDropDown = new DropDownManager(GameActivity.this, settingsList, settingsList.size());
+
         ImageView settings = (ImageView) findViewById(R.id.settings);
         headerParams = new RelativeLayout.LayoutParams(buttonSize, buttonSize);
         headerParams.setMargins(buttonPadding, buttonPadding * 2, buttonPadding, buttonPadding * 2);
         headerParams.addRule(RelativeLayout.ALIGN_PARENT_RIGHT);
         settings.setLayoutParams(headerParams);
-        about.setOnClickListener(new View.OnClickListener() {
+        settings.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                //todo set up settings
+            public void onClick(final View v) {
+                settingsDropDown.popupWindow.showAtLocation(v, Gravity.CENTER, 0, 0);
+                settingsDropDown.popupWindow.setOnDismissListener(new PopupWindow.OnDismissListener() {
+                    @Override
+                    public void onDismiss() {
+                        isRunning = false;
+                        gameStop.setClickable(false);
+                        gameStart.setClickable(true);
+
+                        if (settingsDropDown.getSelected() == 1) {
+                            final DropDownManager cellColorDropDown = new DropDownManager(GameActivity.this, cellColorsList, cellColorsList.size());
+
+                            cellColorDropDown.popupWindow.showAtLocation(v, Gravity.CENTER, 0, 0);
+                            cellColorDropDown.popupWindow.setOnDismissListener(new PopupWindow.OnDismissListener() {
+                                @Override
+                                public void onDismiss() {
+                                    cellColorKey = cellColorDropDown.getSelected();
+                                    setCellColor(cellColorsMap.get(cellColorKey - 1));
+
+                                    initializeGrid();
+                                    lifeManager = new LifeManager(lifeGrid, gridAdapter, boardSize, boardSizeMap.get(boardKey));
+
+                                    try {
+                                        Toast.makeText(GameActivity.this, "Cell color set to: " + cellColorsList.get(cellColorDropDown.getSelected()), Toast.LENGTH_SHORT).show();
+                                    } catch (IndexOutOfBoundsException ignored) {
+                                    }
+                                }
+                            });
+
+                        } else if (settingsDropDown.getSelected() == 2) {
+                            final DropDownManager bgColorDropDown = new DropDownManager(GameActivity.this, bgColorsList, bgColorsList.size());
+
+                            bgColorDropDown.popupWindow.showAtLocation(v, Gravity.CENTER, 0, 0);
+                            bgColorDropDown.popupWindow.setOnDismissListener(new PopupWindow.OnDismissListener() {
+                                @Override
+                                public void onDismiss() {
+                                    bgColorKey = bgColorDropDown.getSelected();
+                                    View root = findViewById(R.id.game_root);
+                                    root.setBackgroundColor(bgColorsMap.get(bgColorKey - 1));
+
+                                    try {
+                                        Toast.makeText(GameActivity.this, "Background color set to: " + bgColorsList.get(bgColorDropDown.getSelected()), Toast.LENGTH_SHORT).show();
+                                    } catch (IndexOutOfBoundsException ignored) {
+                                    }
+                                }
+                            });
+                        }
+
+                        settingsDropDown.setCurrentSelection(aboutList.size());
+
+                    }
+                });
             }
 
         });
@@ -315,7 +456,7 @@ public class GameActivity extends Activity {
      * Initializes features buttons and sets on click listeners
      */
     private void initializeFeaturesButtons() {
-        final DropDownManager sizesDropDown = new DropDownManager(GameActivity.this, sizeList, boardKey);
+        final DropDownManager sizesDropDown = new DropDownManager(GameActivity.this, sizeList, boardKey + 1);
 
         ImageView size = (ImageView) findViewById(R.id.board_size);
         size.setLayoutParams(params);
@@ -326,25 +467,27 @@ public class GameActivity extends Activity {
                 sizesDropDown.popupWindow.setOnDismissListener(new PopupWindow.OnDismissListener() {
                     @Override
                     public void onDismiss() {
-                        boardKey = sizesDropDown.getSelected();
-                        setBoardSize(boardSizeMap.get(boardKey - 1));
-                        isRunning = false;
-                        fillGridArray();
-                        initializeGrid();
-                        lifeManager = new LifeManager(lifeGrid, gridAdapter, boardSize, boardSizeMap.get(boardKey));
-                        gameStop.setClickable(false);
-                        gameStart.setClickable(true);
+                        if (boardKey + 1 != sizesDropDown.getSelected()) {
+                            boardKey = sizesDropDown.getSelected();
+                            setBoardSize(boardSizeMap.get(boardKey - 1));
+                            isRunning = false;
+                            fillGridArray();
+                            initializeGrid();
+                            lifeManager = new LifeManager(lifeGrid, gridAdapter, boardSize, boardSizeMap.get(boardKey));
+                            gameStop.setClickable(false);
+                            gameStart.setClickable(true);
 
-                        try {
-                            Toast.makeText(GameActivity.this, "Board sized to: " + sizeList.get(sizesDropDown.getSelected()), Toast.LENGTH_SHORT).show();
-                        } catch (IndexOutOfBoundsException ignored) {
+                            try {
+                                Toast.makeText(GameActivity.this, "Board sized to: " + sizeList.get(sizesDropDown.getSelected()), Toast.LENGTH_SHORT).show();
+                            } catch (IndexOutOfBoundsException ignored) {
+                            }
                         }
                     }
                 });
             }
         });
 
-        final DropDownManager speedDropDown = new DropDownManager(GameActivity.this, speedList, evolutionKey);
+        final DropDownManager speedDropDown = new DropDownManager(GameActivity.this, speedList, evolutionKey + 1);
         ImageView speed = (ImageView) findViewById(R.id.generation_speed);
         speed.setLayoutParams(params);
         speed.setOnClickListener(new View.OnClickListener() {
@@ -390,7 +533,6 @@ public class GameActivity extends Activity {
                 });
             }
         });
-
     }
 }
 
